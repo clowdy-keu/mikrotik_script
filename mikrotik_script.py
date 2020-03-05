6e7941430f41a21a3c2b06866760434d23adff97
clowdy-keu <56542267+clowdy-keu@users.noreply.github.com>
2020-03-06 00:12:48 +0700

    v 0.2
    1. Исключен повторяющийся код в сегменте ввода пароля.
    2. Добавлена возможность задания таймаута и его увеличение.
    3. Добавлено описание скрипта по -h
    4. Добавлена возможность ввода коротких команд.
    5. Команда собирается отдельно, без тройного join
    6. От наполнение warning_params отказались, если сравнение параметров не проходит - просто ругаемся.
    7. pass в except убрали.
    8. Глобально изменена логика проверки параметров:
    - в .csv сделаны 3 колонки (параметр, минимум и максимум)
    - интересные нам параметры приводятся к строго численно сравнимому виду, вне зависимости от их типа
    - параметр и границы для сравнения передаются в функцию, которая решает, все ли у нас хорошо, ей без разницы, какой именно параметр мы сравниваем.
    Т.о. можем безболезненно добавлять новые параметры для сравнения, нужно лишь добавить их в словарь соответствий и описать парсинг до сравнимого значения.
    Самые поверхностные моменты ошибок пользователя постарались предусмотреть (н-р неправильные единицы измерения в памяти, неверный формат аптайма, лишние или недостающие параметры, ошибки в именовании параметров и т.д.)
    TODO:
    1. Парсинг логин@адрес@порт
    2. Перезагрузка порта
    3. Подключение по telnet
    4. Инструмент работы с файлом паролей

--- mikrotik_script.py
+++ mikrotik_script.py
@@ -2,7 +2,6 @@ import argparse
 import socket
 import base64
 import re
-from distutils.version import LooseVersion
 
 
 from paramiko import AutoAddPolicy, SSHClient
@@ -11,11 +10,12 @@ from paramiko.ssh_exception import (AuthenticationException, NoValidConnectionsE
 
 class SSHMikrotik(object):
 
-    def __init__(self, address, port, username, password=None):
+    def __init__(self, address, port, username, password=None, timeout=4):
         self.address = address
         self.port = port
         self.username = username
         self.password = password
+        self.timeout = timeout
         self.connection = self.connect()
 
     def __enter__(self):
@@ -43,41 +43,27 @@ class SSHMikrotik(object):
         mt_ssh.set_missing_host_key_policy(AutoAddPolicy())
 
         #Пользуемся генератором, если пользователь сам не ввел пароль.
-        #Нужно разобраться со структурой, как будто ее можно улучшить здесь.
-        if self.password is None:
-            for next_pass in pass_generator():
-                try:
-                    mt_ssh.connect(self.address, self.port, username=self.username, password=next_pass,
-                                    timeout=1, allow_agent=False, look_for_keys=False)
-
-                except (AuthenticationException, NoValidConnectionsError, SSHException) as error:
-                    print(error)
-
-                except socket.timeout:
-                    print('Connection timeout')
-                    break
-
-                else:
-                    return mt_ssh
+        if self.password:
+            pass_generator = [self.password]
         else:
+            with open("passwords.csv", "r") as pass_file:
+                pass_generator = pass_generator()
+        for next_pass in pass_generator:
             try:
-                mt_ssh.connect(self.address, self.port, username=self.username, password=self.password,
-                            timeout=1, allow_agent=False, look_for_keys=False)
+                mt_ssh.connect(self.address, self.port, username=self.username, password=next_pass,
+                               timeout=self.timeout, allow_agent=False, look_for_keys=False)
             except (AuthenticationException, NoValidConnectionsError, SSHException) as error:
                 print(error)
 
             except socket.timeout:
                 print('Connection timeout')
+                if self.timeout < 10:
+                    self.timeout += 2
 
             else:
                 return mt_ssh
 
     def execute(self, command):
-        try:
-            if isinstance(command, list):
-                command = " ".join(command) 
-        except TypeError:
-            print('Something wrong with your command!')
 
         _, stdout, _ = self.connection.exec_command(command)
 
@@ -98,16 +84,41 @@ class SSHMikrotik(object):
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    address_description = """
+    Address format <username>@<ip-address>:<port>
+    <username>, <port> are optional"""
+    command_description = """
+    You can use short variants of command:
+    uptime->system resource print
+    log->log print
+    export->export compact"""
+    description = """
+    This script is for work with Mikrotik.
+    It has some nice features, so have fun.
+    """
+    password_description = """If you don't enter a password, it will be found in the file."""
+    parser = argparse.ArgumentParser(prog='Script for Mikrotik control',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     description=description)
     #Адрес формата <username>@<ip>:<port> и команда обязательные параметры.
     #У адреса обязательный параметр <ip>, остальные опциональны.
-    parser.add_argument('address', type=str)
-    parser.add_argument('command', type=str, nargs='+')
-    parser.add_argument('-p', '--password', type=str, required=False, default=None)
+    parser.add_argument('address', type=str, help=address_description)
+    parser.add_argument('command', type=str, nargs='+', help=command_description)
+    parser.add_argument('-p', '--password', type=str, required=False, default=None, help=password_description)
+    parser.add_argument('-t', '--timeout', type=int, required=False, default=4)
 
     args = parser.parse_args()
 
-    #Хочется от этого избавиться, нужно погрузиться в argparse.
+    short_commands = {
+        "uptime": "system resource print",
+        "log": "log print",
+        "export": "export compact"
+    }
+
+    command = " ".join(args.command)
+    if command in short_commands:
+        command = short_commands[command]
+
     port = 22
     username = 'Admin'
 
@@ -121,36 +132,23 @@ def main():
 
     #Модификация вывода команды system resource print
     def mod_sys_res_print(output):
-        list_params = list(map(list, zip([item.split(":", 1)[0].replace(" ", "") for item in output.split("\n")],
+        parameters_from_mikrotik = list(map(list, zip([item.split(":", 1)[0].replace(" ", "") for item in output.split("\n")],
                                          [item.split(":", 1)[1].strip() for item in output.split("\n")])))
 
         with open("params_for_sys_res.csv", "r") as params_file:
-            list_need_params = [item.split(',') for item in params_file.read().splitlines()]
+            parameters_for_output = [item.split(',') for item in params_file.read().splitlines()][1::]
+        #Если в файле оказалось некорректное число параметров, приводим к 3 необходимым.
+        for i in range(len(parameters_for_output)):
+            while len(parameters_for_output[i]) < 3:
+                parameters_for_output[i].append('')
+            while len(parameters_for_output[i]) > 3:
+                parameters_for_output[i] = parameters_for_output[i][:3:]
 
         #Проверка, не превышены ли какие-то значения
-        def checking_params(list_params, list_need_params):
+        def checking_params(parameters_from_mikrotik, parameters_for_output):
 
-            #Функции отдельно под каждый параметр со своей логикой парсинга.
-            def uptime_check(checked_param, param):
+            def uptime_conversion(*args):
 
-                sign_compr = re.search(r'\W*', param).group()
-                param = re.sub(sign_compr, '', param)
-
-                time_dict_need = {}.fromkeys("wdhms", 0)
-                time_dict_check = {}.fromkeys("wdhms", 0)
-
-                for sym in "wdhms":
-                    next_time_value_need = re.split(sym, param)
-                    next_time_value_check = re.split(sym, checked_param)
-
-                    if len(next_time_value_need) > 1:
-                        time_dict_need[str(sym)] += int(next_time_value_need[0])
-                        param = next_time_value_need[1]
-
-                    if len(next_time_value_check) > 1:
-                        time_dict_check[str(sym)] += int(next_time_value_check[0])
-                        checked_param = next_time_value_check[1]
-                #Переводим в секунды для сравнения значений
                 def dict_wdhms_in_sec(time_dict):
                     seconds = 0
                     for item in time_dict:
@@ -166,106 +164,127 @@ def main():
                             seconds += int(time_dict[item]) * 60 * 60 * 24 * 7
                     return seconds
 
-                time_sec_check = dict_wdhms_in_sec(time_dict_check)
-                time_sec_need = dict_wdhms_in_sec(time_dict_need)
-
-                #Этот блок хочется сделать элегантнее, напрямую используя sign_compr в котором храним знак сравнения
-                #чтобы не городить иерархию
-                result = True
-                if sign_compr == '<':
-                    result = time_sec_check < time_sec_need
-                elif sign_compr == '<=':
-                    result = time_sec_check <= time_sec_need
-                elif sign_compr == '>':
-                    result = time_sec_check > time_sec_need
-                elif sign_compr == '>=':
-                    result = time_sec_check >= time_sec_need
-                elif sign_compr == '=':
-                    result = time_sec_check == time_sec_need
-                else:
-                    print("Wrond comparison sign format")
-
+                result = [''] * len(args)
+                for i, parameter in enumerate(args):
+                    if parameter != '':
+                        if re.match(r"(\d+[wdhms])+", parameter).group() == parameter:
+                            time_dict = {}.fromkeys("wdhms", 0)
+                            for sym in "wdhms":
+                                if re.search(sym, parameter) is not None:
+                                    next_value = re.split(sym, parameter)
+                                    time_dict[str(sym)] += int(next_value[0])
+                                    parameter = next_value[1]
+                            time_sec = dict_wdhms_in_sec(time_dict)
+                            result[i] = int(time_sec)
+                        else:
+                            print("Something wrong with uptime parameter, please check it")
+                            result[i] = -1
+                return result
+            #Версии дело тонкое, если там будут условные beta alpha - это нужно будет договариваться
+            #какая версия больше/меньше и допиливать.
+            #Так как сравнение идет в универсальной функции - нам нужны численно сравнимые значения
+            #Придумал так: добиваем номер версии нулями до формата #.#.#.#.#.#, так чтобы с запасом
+            #Формируем большое число(например 600450008000000000000), которое на самом деле
+            #имеет формат 6.0045.0008.0000.0000.0000, то есть в каждом разряде есть запас под
+            #9999 подверсий (в первом нули отбрасываются). Их сравниваем.
+            def version_conversion(*args):
+                result = [''] * len(args)
+                for i, parameter in enumerate(args):
+                    if parameter != '':
+                        version = re.search(r"[0-9.]*", parameter).group().split('.')
+                        while len(version) < 6:
+                            version.append('0')
+                        version = int(''.join([item.zfill(4) for item in version]))
+                        result[i] = int(version)
+                return result
+            #Учитываем единицы измерения, переводим все в KiB
+            def memory_and_hdd_coversion(*args):
+                result = [''] * len(args)
+                for i, parameter in enumerate(args):
+                    if parameter != '':
+                        unit_dict = {'KiB': 0, 'MiB': 1, 'GiB': 2, 'TiB': 3, 'PiB': 4, 'EiB': 5}
+                        unit = re.search(r"[A-Za-z]+", parameter)
+                        if unit is not None:
+                            unit = unit.group()
+                        else:
+                            unit = ''
+                        memory = float(re.search(r"[^A-Za-z]*", parameter).group())
+                        if unit in unit_dict:
+                            memory *= 2 ** (10 * unit_dict[unit])
+                            result[i] = int(memory)
+                        else:
+                            print("Please check out memory's unit of measurement")
+                            result[i] = -1
                 return result
 
-            def version_check(checked_param, param):
-
-                sign_compr = re.search(r'[<>=]*', param).group()
-                param = re.sub(sign_compr, '', param)
-
-                need_version = re.search(r"[0-9.]*", param).group()
-                check_version = re.search(r"[0-9.]*", checked_param).group()
-
-                result = True
-                if sign_compr == '<':
-                    result = LooseVersion(check_version) < LooseVersion(need_version)
-                elif sign_compr == '<=':
-                    result = LooseVersion(check_version) <= LooseVersion(need_version)
-                elif sign_compr == '>':
-                    result = LooseVersion(check_version) > LooseVersion(need_version)
-                elif sign_compr == '>=':
-                    result = LooseVersion(check_version) >= LooseVersion(need_version)
-                elif sign_compr == '=':
-                    result = LooseVersion(check_version) == LooseVersion(need_version)
-                else:
-                    print("Wrond comparison sign format")
-
+            def cpu_conversion(*args):
+                result = [''] * len(args)
+                for i, parameter in enumerate(args):
+                    if parameter != '':
+                        cpu = int(re.search(r"\d+", parameter).group())
+                        result[i] = int(cpu)
                 return result
-            #пустышки
-            def memory_check(checked_param, param):
-                return False
-
-            def cpu_check(checked_param, param):
-                return False
-
-            def hdd_check(checked_param, param):
-                return False
-
-            #Задаем соответствие между интересными параметрами и функциями-обработчиками
-            controlled_params = {
-                "uptime": uptime_check,
-                "version": version_check,
-                "free-memory": memory_check,
-                "cpu-load": cpu_check,
-                "free-hdd-space": hdd_check
+            #Совсем без деревьев не обошлось, если в min/max value приходят пустые значения
+            #нужно как-то с ними поступать, двойное сравнение не сработает.
+            def is_value_in_range(*args):
+                check_value, min_value, max_value = args
+                if min_value == '' and max_value == '':
+                    return True
+                if min_value == '':
+                    return check_value <= max_value
+                if max_value == '':
+                    return check_value >= min_value
+                if min_value > max_value:
+                    print("Minimum value is bigger than maximum, check it")
+                    return False
+                else:
+                    return min_value <= check_value <= max_value
+            #Функции исключительно конвертируют, ничего не знаю о том, что именно им приходит
+            parameters_for_control = {
+                "uptime": uptime_conversion,
+                "version": version_conversion,
+                "free-memory": memory_and_hdd_coversion,
+                "cpu-load": cpu_conversion,
+                "free-hdd-space": memory_and_hdd_coversion
             }
 
-            warning_params = []
-            for param in list_need_params:
-                if param[0] in controlled_params:
-                    for item in list_params:
-                        if item[0] == param[0]:
-                            checked_param = item
-                            break
+            for parameter, min_value, max_value in parameters_for_output:
+                try:
+                    value_for_check = \
+                    parameters_from_mikrotik[[item[0] for item in parameters_from_mikrotik].index(parameter)][1]
+                    if parameter in parameters_for_control:
+                        pack_for_coversion = parameters_for_control.get(parameter)\
+                            (value_for_check, min_value, max_value)
+                        if not is_value_in_range(*pack_for_coversion):
+                            print("Controlled parameter {} = {} is out of range min({}) - max({})"
+                                  .format(parameter, value_for_check, min_value, max_value))
                     else:
-                        print("Controlled parameter '{}' is absent in the output".format(param[0]))
                         continue
-                    if controlled_params.get(param[0])(checked_param[1], param[1]):
-                        warning_params.append(checked_param[0])
+                except ValueError:
+                    print("Controlled parameter '{}' is absent in the output ".format(parameter))
 
-            return (warning_params)
-
-        dict_need_params = {item[0]: item[1] for item in list_params if
-                            item[0] if item[0] in [item[0] for item in list_need_params]}
-
-        for key, value in dict_need_params.items():
+        dict_need_params = [item for item in parameters_from_mikrotik if
+                            item[0] in [item[0] for item in parameters_for_output]]
+        #Выводим интересное
+        for key, value in dict_need_params:
             print("%-15s: %25s" % (key, value))
 
-        for warning in checking_params(list_params, list_need_params):
-            print("{} is out of range".format(warning))
-
-    #Задаем соответствие между командами, которым нужен оригинальный вывод и функциями-обработчиками
+        #Проверяем соответствие границам и показываем warning_message
+        checking_params(parameters_from_mikrotik, parameters_for_output)
+    #Задаем соответствие между командами, которым нужен оригинальный вывод и функциями-обработчиками.
     modified_commands = {
         "system resource print": mod_sys_res_print
     }
 
-    with SSHMikrotik(args.address, port, username, args.password) as mt_ssh:
+    with SSHMikrotik(args.address, port, username, args.password, args.timeout) as mt_ssh:
         try:
-            #Если нужен оригинальный вывод, используем функцию-обработчик
-            if " ".join(args.command) in modified_commands:
-                modified_commands.get(" ".join(args.command))(mt_ssh.execute(args.command))
+            #Если нужен оригинальный вывод, используем функцию-обработчик.
+            if command in modified_commands:
+                modified_commands.get(command)(mt_ssh.execute(command))
             else:
-                print(mt_ssh.execute(args.command))
+                print(mt_ssh.execute(command))
         except Exception:
-            pass
+            print("Something terrible happened")
+
 if __name__ == '__main__':
     main()
